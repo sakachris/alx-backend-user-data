db8de2aa5f6a03771c34fab5c2308570b8f67540
sakachris <sakachris90@mail.com>
2024-02-07 18:30:48 +0300

    regexs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,12 +2,23 @@
 ''' filtered_logger.py '''
 
 import re
+from typing import List
+
+
+def extract_pattern(fields: List[str], separator: str) -> str:
+    """Generate the regular expression pattern for extracting fields."""
+    return rf'(?P<field>{ "|".join(fields) })=[^{separator}]*'
+
+
+def replace_pattern(redaction: str) -> str:
+    """Generate the replacement pattern."""
+    return rf'\g<field>={redaction}'
 
 
 def filter_datum(
-        fields: list[str], redaction: str, message: str, separator: str
-) -> str:
-    for field in fields:
-        pattern = rf'({field}=)[^{separator}]*'
-        message = re.sub(pattern, rf'\1{redaction}', message)
-    return message
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line."""
+    extract = extract_pattern(fields, separator)
+    replace = replace_pattern(redaction)
+    return re.sub(extract, replace, message)
