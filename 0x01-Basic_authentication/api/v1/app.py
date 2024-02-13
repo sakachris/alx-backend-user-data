51f03229c1d18351c46cc7c38e99424544a801d6
sakachris <sakachris90@mail.com>
2024-02-13 20:34:28 +0300

    basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,7 +4,8 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
+# from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -16,8 +17,14 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
 
-if getenv("AUTH_TYPE") == 'auth':
+if getenv("AUTH_TYPE") == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
     auth = Auth()
+# if getenv("AUTH_TYPE") == 'auth':
+# auth = Auth()
 
 
 @app.errorhandler(404)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+''' api/v1/auth/basic_auth.py '''
+
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    ''' Inheriting Auth class '''
+    pass
