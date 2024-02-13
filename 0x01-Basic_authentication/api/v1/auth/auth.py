c2f5cad024b1738230acd15114549cbcc1bd39bd
sakachris <sakachris90@mail.com>
2024-02-13 13:52:05 +0300

    routes not needing authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,19 @@ class Auth:
     ''' Auth class '''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if authentication is required for the given path."""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path.endswith('/'):
+            path = path[:-1]
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                excluded_path = excluded_path[:-1]
+            if path == excluded_path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Get the Authorization header from the request."""
