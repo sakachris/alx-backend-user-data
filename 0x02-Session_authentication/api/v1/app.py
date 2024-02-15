8fbbb52d6f43b2143a8a09a5de1896eccdcff790
sakachris <sakachris90@mail.com>
2024-02-15 07:16:22 +0300

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -53,12 +53,16 @@ def auth_before_request():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/',
         ]
         if auth.require_auth(request.path, excluded_paths):
             auth_header = auth.authorization_header(request)
-            user = auth.current_user(request)
-            if auth_header is None:
+            session_cookie = auth.session_cookie(request)
+            if auth_header is None and session_cookie is None:
                 abort(401)
+            user = auth.current_user(request)
+            '''if auth_header is None:
+                abort(401)'''
             if user is None:
                 abort(403)
             request.current_user = user
