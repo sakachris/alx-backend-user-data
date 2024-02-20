777bbb553312e3c6e823b2496aa94595ec385192
sakachris <sakachris90@mail.com>
2024-02-20 13:30:55 +0300

    reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -89,3 +89,14 @@ class Auth:
         self._db.update_user(user.id, reset_token=reset_token)
 
         return reset_token
+
+    def update_password(self, reset_token: str, new_password: str) -> None:
+        """Update user's password using the reset token."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError()
+
+        hashed_password = _hash_password(new_password)
+        self._db.update_user(
+                user.id, hashed_password=hashed_password, reset_token=None)
