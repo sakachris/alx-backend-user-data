12a4400b19c034d18e92fac990ed39cf5d38211a
sakachris <sakachris90@mail.com>
2024-02-20 07:50:40 +0300

    log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 ''' app.py '''
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 from typing import Dict, Any
 
@@ -26,6 +26,21 @@ def users() -> Dict[str, Any]:
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login() -> Dict[str, Any]:
+    ''' endpoint to log in user '''
+    email: str = request.form.get('email')
+    password: str = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response, 200
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
